c19ad0a09c877ce8935840cbdddef2515e065760
creator64 <armand.ayar@outlook.com>
2022-04-17 15:09:24 +0100

    eliminated collection from this version

--- npt/settings.py
+++ npt/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main.apps.MainConfig',
-    'collection.apps.CollectionConfig',
+    #'collection.apps.CollectionConfig',
 ]
 
 MIDDLEWARE = [
