3bc25546048788e5c8b1cef37d0878d0b708d138
creator64 <armand.ayar@outlook.com>
2022-04-17 17:27:18 +0100

    attempts

--- npt/settings.py
+++ npt/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "/static/"
+STATIC_ROOT = "/static"
 
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
